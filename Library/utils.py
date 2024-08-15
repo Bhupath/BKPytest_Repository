a044fe87a8594e5d5c987868b805e11771dbd5f0
bk-runner-7 <53376435+Bhupath@users.noreply.github.com>
2024-08-15 18:46:36 +0530

    Update utils.py

--- Library/utils.py
+++ Library/utils.py
@@ -1,1 +1,21 @@
+#/usr/bin/python
+
+# General util specific to common purpose
+
+class YAML:
+  @staicmethod
+  def yaml_to_dict(filepath: str) -> dict:
+    with(filepath, "r") as fileobj:
+      data = yaml.safe_load(fileobj)
+    return data
+  @staticmethod
+  def write_data_to_yaml():
+    pass
+
+class DateTime:
+  def __init__(self):
+    pass
+  def getCurrentTime(self):
+    pass
+
 
