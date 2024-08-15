046702c91a1f0579fc845777b9e4ff6ecf423092
bk-runner-7 <53376435+Bhupath@users.noreply.github.com>
2024-08-15 18:43:46 +0530

    Update sshhandler.py

--- Library/sshhandler.py
+++ Library/sshhandler.py
@@ -1,1 +1,31 @@
+#/usr/bin/python
+
+import shutil
+import subprocess
+import paramiko
+
+class ExecuteSSHCommands:
+
+  def __init__(self):
+    pass
+
+  @staticmethod
+  def execute_command(cmd: str) -> str:
+    """
+    * Documentation:
+    param cmd: str
+    return output.stdout: str
+    """
+    output = None
+    try:
+      output = subprocess.run(cmd, shell=True, text=True, capture_output=True)
+      assert output.return_code == 0, "Command Execution is Failed"
+    except AssertionError as err:
+      print(f"Exception was occured due to {err}")
+      return output
+    return output.stdout
+  @staticmethod
+  def exeucte_remote_command (cmd: str, remote_ip: str, username: str, password: str) -> str:
+    output = None
+    #paramiko object to implement
 
