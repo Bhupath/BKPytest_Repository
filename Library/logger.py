76ba16a8ebd7d3a62117d0dae099279f7bf0c0c9
bk-runner-7 <53376435+Bhupath@users.noreply.github.com>
2024-08-15 18:37:52 +0530

    Update logger.py

--- Library/logger.py
+++ Library/logger.py
@@ -6,4 +6,10 @@ import logging
 def createLogger():
   logger = logging.getLogger(__name__)
   log_file_path = f"output_{datetime.strftime(datetime.now(),'%H_%M_%S')}.log"
-  formatter = logging.Formatter("[%(asctime)s][%(name)s][]")
+  formatter = logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s][%(filename)s][%(funcName)s:%(lineno)d]: %(message)s")
+  fileHandler = loggin.FileHandler(log_file_path)
+  fileHandler.setFormatter(formatter)
+  logging.addHandler(fileHandler)
+  logging.setLeve(logging.DEBUG)
+  return logger
+  
