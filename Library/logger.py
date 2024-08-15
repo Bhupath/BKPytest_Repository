19ba9721a990a7f4b4bff269b3cf039167b3971b
bk-runner-7 <53376435+Bhupath@users.noreply.github.com>
2024-08-15 18:34:15 +0530

    Update logger.py

--- Library/logger.py
+++ Library/logger.py
@@ -1,1 +1,9 @@
+#/usr/bin/python
+import os
+from datetime import datetime
+import logging
 
+def createLogger():
+  logger = logging.getLogger(__name__)
+  log_file_path = f"output_{datetime.strftime(datetime.now(),'%H_%M_%S')}.log"
+  formatter = logging.Formatter("[%(asctime)s][%(name)s][]")
