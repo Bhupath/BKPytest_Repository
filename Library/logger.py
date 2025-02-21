b2f533cdd3980e02e53c10aa569f2f6cf557c106
bk-runner-7 <53376435+Bhupath@users.noreply.github.com>
2025-02-21 17:47:02 +0530

    Update logger.py
    Updated

--- Library/logger.py
+++ Library/logger.py
@@ -1,15 +1,25 @@
-#/usr/bin/python
+import logging.handlers
 import os
-from datetime import datetime
+import sys
+import time
 import logging
+import input
 
-def createLogger():
-  logger = logging.getLogger(__name__)
-  log_file_path = f"output_{datetime.strftime(datetime.now(),'%H_%M_%S')}.log"
-  formatter = logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s][%(filename)s][%(funcName)s:%(lineno)d]: %(message)s")
-  fileHandler = loggin.FileHandler(log_file_path)
-  fileHandler.setFormatter(formatter)
-  logging.addHandler(fileHandler)
-  logging.setLeve(logging.DEBUG)
-  return logger
-  
+def createLogger()->logging.Logger:
+    if not os.path.exists(input.sanity_logs):
+        os.makedirs(input.sanity_logs)
+    logfile=(input.sanity_logs + '/SanityExecution.log')
+    print(logfile)
+    logger = logging.getLogger(__name__)
+    if logger.hasHandlers():
+        logger.handlers = []
+    formatter=logging.Formatter('[%(asctime)s][%(name)s][%(levelname)s][%(filename)s][%(funcName)s:%(lineno)d]: %(message)s')
+    fileHandler = logging.FileHandler(logfile)
+    fileHandler.setFormatter(formatter)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(formatter)
+    logger.setLevel(logging.DEBUG)
+    #logger.propagate = False
+    logger.addHandler(fileHandler)
+    logger.addHandler(consoleHandler)
+    return logger
